08744202e121a2e636122f6541295c9a623fdc4b
Michael Orji <orjimichael@4886@gmail.com>
2025-08-31 09:11:29 +0100

    Refactor Sentry configuration to use environment variable for DSN

--- sentry.config.ts
+++ sentry.config.ts
@@ -1,13 +1,9 @@
 import Sentry from '@sentry/node';
-
-// Sentry.init({
-//   dsn: process.env.SENTRY_DSN,
-//   sendDefaultPii: true,
-//   tracesSampleRate: 1.0,
-// });
+import dotenv from 'dotenv';
+dotenv.config();
 
 Sentry.init({
-  dsn: 'https://6395f4ae18857cbfa475190e06ad1df4@o4505816031428608.ingest.us.sentry.io/4509933537263616',
+  dsn: process.env.SENTRY_DSN,
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
